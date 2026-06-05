367cbaa9ea9d3e16558d07585bc6062055be5a6a
sherril203 <sherrilkumar2003@gmail.com>
2026-06-05 23:35:18 +0530

    two pages created and updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
+const path = require('path');
+
+require('dotenv').config({ path: path.resolve(__dirname, '../.env.local') });
+
 const mongoose = require('mongoose');
 const server = require('./app');
-require('dotenv').config();
 
 const mongodb_uri = process.env.MONGODB_URI;
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
+
 
 if (!mongodb_uri) {
   console.error("MONGODB_URI is not defined ");
@@ -12,11 +16,11 @@ if (!mongodb_uri) {
 
 mongoose.connect(mongodb_uri)
   .then(() => {
-    console.log(" MongoDB connected");
+    console.log(" MongoDB connected successfully!");
     server.listen(PORT, () => {
       console.log(` Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.error(" MongoDB connection error:", err);
+    console.error("MongoDB connection error:", err);
   });
